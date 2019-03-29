43a2ad83bffec889af8a7ddc6aa94875db08967e
chihaojie <chihaojie@koalareading.com>
2019-03-29 18:48:34 +0800

    初始环境搭建

---
+++ es_in_practice/src/main/java/com/StartApplication.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+
+/**
+ * @Author chihaojie
+ * @Description 启动类
+ * @date 2019/3/29   18:42
+ **/
+@SpringBootApplication
+@MapperScan("com.dao")
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+        System.out.println(".................ES微服务启动完成...............");
+    }
+}
